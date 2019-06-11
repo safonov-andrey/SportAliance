ef5d1d07fdd9f3ea0fcd79f0b51b08e2e70bd59e
ehob <safonov.andrey.eho@gmail.com>
2019-06-11 09:45:46 +0200

    Sport Aliance test tasks

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log('Hello SportAliance!');
+(function test1() {
+
+    function reverse(str) {
+        var result = '';
+        for (var i = str.length; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+        return result.toLowerCase();
+    }
+
+    console.log(reverse('Hello World') === 'dlrow olleh');
+
+})();
+
+(function test2() {
+
+    function sumNum(arr) {
+        return arr
+            .filter(item => typeof item === 'number')
+            .reduce((prev, next) => prev + next, 0);
+    }
+
+    console.log(sumNum([true, 2, 3, '5', 9]) === 14);
+
+})();
+
+(function test4() {
+
+    function sumNum(num) {
+        let result = 0;
+        new Array(num + 1).fill(1).map((item, i) => {
+            result += i
+        });
+        
+        return result;
+    }
+    console.log(sumNum(5));
+    console.log(sumNum(5) === 15);
+
+})();
